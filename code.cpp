560f78daf967236ce2733dd89c6b9b1cc1621560
FIRST_NAME LAST_NAME <khadijarejjaoui41@gmail.com>
2022-02-23 23:26:53 +0600

    first commit

---
+++ code.cpp
@@ -0,0 +1,23 @@
+// solution of 1037 Interval problem in beecrowd
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    double X;
+    cin >> X;
+    if(X < 0 || X >100){
+        cout << "Fora de intervalo"<<endl;
+    }else if(X <=25){
+       cout<< "Intervalo [0,25]"<<endl;
+    }else if(X <=50){
+       cout<< "Intervalo (25,50]"<<endl;
+    }
+    else if(X <=75){
+       cout<< "Intervalo (50,75]"<<endl;
+    }else{
+        cout<< "Intervalo (75,100]"<<endl;
+    }
+    
+    system("pause");
+    return 0;
+}
